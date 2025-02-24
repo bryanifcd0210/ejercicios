ca049df19131d173bfa893d7723fe3f1777553d0
bryanifcd0210 <fcdbryan5@gmail.com>
2025-02-24 10:47:38 +0100

    ejercicio 4: funcion àra imprimir tabla de mult. del 2

---
+++ ejercicio_4.php
@@ -0,0 +1,10 @@
+<?php
+function multiplicar() {
+    for ($i = 0; $i <= 10; $i++) {
+        $contador = $i * 2;
+        echo "2 x " . $i . " = " . $contador . "<br>";
+    }
+}
+
+multiplicar();
+?>
